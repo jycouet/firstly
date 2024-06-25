b8de1438dd345c85368153479f38a43b7e03b0f6
jycouet <jycouet@gmail.com>
2024-06-26 00:35:00 +0200

    new generate

---
+++ packages/remult-kit/src/lib/auth/static/assets/Page-Bz5t3fND.js
@@ -0,0 +1,1 @@
+import{S as o,b as r,a as l,H as i,d as c,n as a,k as f}from"./index-BYgmIMJv.js";function u(s){let t;return{c(){t=i("Hello from files")},m(e,n){c(e,t,n)},p:a,i:a,o:a,d(e){e&&f(t)}}}class p extends o{constructor(t){super(),r(this,t,null,u,l,{})}}export{p as default};
---
+++ packages/remult-kit/src/lib/auth/static/assets/Page-DAiiQTwn.js
@@ -0,0 +1,1 @@
+import{S as o,b as r,a as i,H as l,d as c,n as a,k as f}from"./index-BYgmIMJv.js";function m(s){let t;return{c(){t=l("Hello from admin")},m(e,n){c(e,t,n)},p:a,i:a,o:a,d(e){e&&f(t)}}}class d extends o{constructor(t){super(),r(this,t,null,m,i,{})}}export{d as default};
