3e85fe4bb140ff0e684ba009b569df5a0de2ec31
jycouet <jycouet@gmail.com>
2024-06-30 18:00:49 +0200

    rename file to "firstly"

--- packages/firstly/src/hooks.server.ts
+++ packages/firstly/src/hooks.server.ts
@@ -1,6 +1,6 @@
 import { sequence } from '@sveltejs/kit/hooks'
 
 import { remultKit } from './lib/handle/index.js'
-import { remultApi } from './routes/api/[...remult]/remult-kit.js'
+import { remultApi } from './routes/api/[...remult]/firstly.js'
 
 export const handle = sequence(remultKit(remultApi))
--- packages/firstly/src/routes/api/[...remult]/+server.ts
+++ packages/firstly/src/routes/api/[...remult]/+server.ts
@@ -1,3 +1,3 @@
-import { remultApi } from './remult-kit'
+import { remultApi } from './firstly'
 
 export const { GET, POST, PUT, DELETE } = remultApi.server
