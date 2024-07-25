7bf53e58b25c39196491e010067091c3a77c6797
jycouet <jycouet@gmail.com>
2024-07-25 14:30:33 +0200

    cleanup @jridgewell

--- packages/firstly/src/lib/vite/index.ts
+++ packages/firstly/src/lib/vite/index.ts
@@ -4,13 +4,7 @@ import { stripper } from 'vite-plugin-stripper'
 
 // import { Log } from '@kitql/helpers'
 
-const toRemove = [
-  'oslo/password',
-  'oslo',
-  '@kitql/internals',
-  '$env/dynamic/private',
-  '@jridgewell/sourcemap-codec',
-]
+const toRemove = ['oslo/password', 'oslo', '@kitql/internals']
 
 export function firstly<KIT_ROUTES extends RouteMappings>(options?: {
   stripper?: { debug?: boolean }
