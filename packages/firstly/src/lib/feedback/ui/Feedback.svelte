ff3fd41dab993feab858c6cc0479c931b2de5ca9
jycouet <jycouet@gmail.com>
2024-10-30 11:11:42 +0100

    Feedback improvements

--- packages/firstly/src/lib/feedback/ui/Feedback.svelte
+++ packages/firstly/src/lib/feedback/ui/Feedback.svelte
@@ -5,10 +5,12 @@
   const list = async () => {
     await dialog.show({
       component: DialogMilestones,
-      classes: { root: 'overflow-auto w-5/6 h-5/6' },
+      classes: { root: 'overflow-auto w-[90vh] h-[90vh]' },
       detail: { caption: 'Feedback' },
     })
   }
 </script>
 
 <button class="text-secondary" on:click={list}>Feedback</button>
+
+<!-- <span class="h-9/12 hidden w-9/12"></span> -->
