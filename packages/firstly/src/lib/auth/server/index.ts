e452c19dd071bbe7123b4c6747682ae0cef7ff79
jycouet <jycouet@gmail.com>
2025-03-05 22:59:46 +0100

    better level of export

--- packages/firstly/src/lib/auth/server/index.ts
+++ packages/firstly/src/lib/auth/server/index.ts
@@ -1,8 +1,9 @@
 import * as arctic from 'arctic'
+import { handleAuth } from './handleAuth.js'
 
 export { auth, authModuleRaw } from './module'
 
 export { checkOAuthConfig } from './providers/helperProvider'
 export { github } from './providers/github'
 
-export { arctic }
+export { arctic, handleAuth }
