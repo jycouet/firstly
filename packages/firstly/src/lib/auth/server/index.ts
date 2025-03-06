eb0d23a4d8399d691e0cbbea31cae0a60d501785
jycouet <jycouet@gmail.com>
2025-03-06 07:58:42 +0100

    format

--- packages/firstly/src/lib/auth/server/index.ts
+++ packages/firstly/src/lib/auth/server/index.ts
@@ -2,6 +2,7 @@ import * as arctic from 'arctic'
 
 import { handleAuth } from './handleAuth.js'
 import { handleGuard } from './handleGuard.js'
+
 export { auth, authModuleRaw } from './module'
 
 export { checkOAuthConfig } from './providers/helperProvider'
