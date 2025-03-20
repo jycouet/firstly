78407e96d3d441cfa761213e8f482dcbbb70588e
jycouet <jycouet@gmail.com>
2025-03-20 14:05:12 +0100

    fix enum

--- packages/firstly/src/modules/task/TaskTypeEnum.ts
+++ packages/firstly/src/modules/task/TaskTypeEnum.ts
@@ -1,24 +1,10 @@
-import { BaseEnum, LibIcon_Add, LibIcon_Delete, type BaseEnumOptions } from "../../lib"
+import { BaseEnum, type BaseEnumOptions } from "../../lib/BaseEnum.js"
+import { LibIcon_Add, LibIcon_Delete } from "../../lib/ui/LibIcon.js"
 import { ValueListFieldType } from "remult"
 import type { Task } from "./Task"
 
-// FIXME: jyc todo
-// @ValueListFieldType()
-// export class TaskTypeEnum extends BaseEnum {
-// 	static EASY = new TaskTypeEnum('EASY', {
-// 		caption: 'Easy',
-// 		icon: { data: LibIcon_Add },
-// 	})
-// 	static HARD = new TaskTypeEnum('HARD', {
-// 		caption: 'Hard',
-// 		icon: { data: LibIcon_Delete },
-// 	})
-// 	constructor(id: string, o?: BaseEnumOptions<Task>) {
-// 		super(id, o)
-// 	}
-// }
 @ValueListFieldType()
-export class TaskTypeEnum {
+export class TaskTypeEnum extends BaseEnum {
 	static EASY = new TaskTypeEnum('EASY', {
 		caption: 'Easy',
 		icon: { data: LibIcon_Add },
@@ -28,6 +14,6 @@ export class TaskTypeEnum {
 		icon: { data: LibIcon_Delete },
 	})
 	constructor(id: string, o?: BaseEnumOptions<Task>) {
-		
+		super(id, o)
 	}
 }
