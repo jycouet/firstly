c21154bafd233b6a7d8a8a0d7e4a28d0b69570df
jycouet <jycouet@gmail.com>
2025-04-03 15:20:11 +0200

    Revert "tmp remove"
    This reverts commit f0a0dbb25ee1d772883b31d4bcf960d8d7f78d20.

--- packages/firstly/src/routes/+layout.svelte
+++ packages/firstly/src/routes/+layout.svelte
@@ -34,7 +34,7 @@
 	const links = [
 		{ path: route('/'), text: 'Home' },
 		{ path: route('/auth'), text: 'Manual Auth' },
-		// { path: route('/mail'), text: 'Mail' },
+		{ path: route('/mail'), text: 'Mail' },
 		{ path: route('/ui/dialog'), text: 'UI / Dialog' },
 		{ path: route('/ui/enum'), text: 'UI / Enum' },
 		{ path: route('/ui/fieldGroup'), text: 'UI / FieldGroup' },
---
+++ packages/firstly/src/routes/mail/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { PageProps } from './$types'
+
+	const { data }: PageProps = $props()
+</script>
+
+<section>
+	<h2 class="text-2xl font-bold">HTML</h2>
+	<div class="border border-primary">
+		{@html data.html}
+	</div>
+</section>
+
+<section class="mt-4">
+	<h2 class="text-2xl font-bold">PLAIN TEXT</h2>
+	<div class="border border-primary">
+		<p>{data.plainText}</p>
+	</div>
+</section>
