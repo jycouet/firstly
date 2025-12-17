d833af8ad6996c821d9f41cb7e9685aba30854cc
jycouet <jycouet@gmail.com>
2025-12-17 14:34:52 +0100

    feedback to native remult module

--- packages/firstly/src/lib/feedback/server/index.ts
+++ packages/firstly/src/lib/feedback/server/index.ts
@@ -1,12 +1,12 @@
 import { remult } from 'remult'
+import { Module } from 'remult/server'
 
-import { ModuleFF } from '../../server'
 import { FeedbackController } from '../FeedbackController'
 import type { FeedbackOptions } from '../types'
 
-export const feedback: (o: FeedbackOptions) => ModuleFF = (o) => {
-	return new ModuleFF({
-		name: 'feedback',
+export const feedback: (o: FeedbackOptions) => Module<unknown> = (o) => {
+	return new Module({
+		key: 'feedback',
 		controllers: [FeedbackController],
 		initRequest: async (kitEvent, op) => {
 			remult.context.feedbackOptions = o
