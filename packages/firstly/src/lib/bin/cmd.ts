b61ba00d143e5fcbbbc091bd27c64005832ef0b1
jycouet <jycouet@gmail.com>
2026-01-18 23:39:40 +0100

    rmv vitePreprocess

--- packages/firstly/src/lib/bin/cmd.ts
+++ packages/firstly/src/lib/bin/cmd.ts
@@ -221,13 +221,9 @@ export default defineConfig({
 	],
 	'./svelte.config.js': [
 		`import adapter from '@sveltejs/adapter-auto'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
