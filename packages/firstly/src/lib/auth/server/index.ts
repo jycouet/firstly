54f44605c9604c5eb0acac18e09f72fa97b9e6e3
jycouet <jycouet@gmail.com>
2026-02-23 11:17:17 +0100

    add a few export

--- packages/firstly/src/lib/auth/server/index.ts
+++ packages/firstly/src/lib/auth/server/index.ts
@@ -5,6 +5,8 @@ import { handleGuard } from './handleGuard.js'
 
 export { auth, authModuleRaw } from './module'
 export { linkRoleToUsersFromEnv as initRoleFromEnv } from './helperRole'
+export { ff_createSession } from './helperFirstly'
+export { deleteSessionTokenCookie } from './helperRemultServer'
 
 export { checkOAuthConfig } from './providers/helperProvider'
 export { github } from './providers/github'
